c2f8ed78838c4dd915010b8f8aaadb5a1b7506aa
RaikkoP <raikko.prants@gmail.com>
2024-01-24 16:25:16 +0200

    Create error handling for unit and integration tests

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = express();
 const mongodb = require('./mongodb/mongodb.connect');
 
 mongodb.connect();
+
 app.use(express.json());
+
+app.use((error, req, res, next) => {
+    res.status(500).json({message: error.message})
+})
 
 app.use("/todos", todoRoutes);
 
--- mongodb/mongodb.connect.js
+++ mongodb/mongodb.connect.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 async function connect() {
     try {
         await mongoose.connect(
-            "mongodb+srv://riggur:riggur123@kool.pmmlais.mongodb.net/",
-            { useNewUrlParser: true }
+            "mongodb+srv://riggur:riggur123@kool.pmmlais.mongodb.net/"
         );
     } catch (err) {
         console.log(err);
